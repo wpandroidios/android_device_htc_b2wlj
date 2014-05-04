9384060c184fa1652a88bbaaa9512cb0d1b749c3
invisiblek <dan.pasanen@gmail.com>
2014-05-04 10:45:45 -0500

    m8vzw: overlay moved to msm8974-common
    Change-Id: I6a133b7191bc5219f5c70c2cd5c45e149748c0b7

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/m8vzw/full_m8vzw.mk)
 
-PRODUCT_PACKAGE_OVERLAYS += device/htc/m8vzw/overlay
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	BUILD_FINGERPRINT="htc/HTCOneM8vzw/htc_m8wl:4.4.2/KOT49H/315044.11:user/release-keys" \
 	PRIVATE_BUILD_DESC="1.12.605.11 CL315044 release-keys"
