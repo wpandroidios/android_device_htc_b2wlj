bb70fe0690223cb8098bcb6b32a82cb84769430d
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-06 18:16:55 -0600

    sepolicy: updates for cmhw
    * add vibe voltage device and context
    * add kcal device and context
    * add missed amp node
    
    Change-Id: I1855648980e0156fffa646d503fbd44b6f724333

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ BOARD_SEPOLICY_UNION += \
     file.te \
     hcheck.te \
     init.te \
+    kcal_dev.te \
     kernel.te \
     mediaserver.te \
     mm-qcamerad.te \
@@ -180,6 +181,7 @@ BOARD_SEPOLICY_UNION += \
     tap2wake_dev.te \
     thermal-engine.te \
     ueventd.te \
+    vibe_dev.te \
     vold.te
 
 # Vendor Init
