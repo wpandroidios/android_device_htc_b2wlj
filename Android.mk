2f6a1ebad05597fa87a585dc6512b7f02014f0f3
scoty755 <scoty755@gmail.com>
2014-12-14 10:45:47 +0900

    Corrected link path of WCNSS_qcom_cfg.ini

--- Android.mk
+++ Android.mk
@@ -78,7 +78,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_SYMLINKS)
 # Create a link for the WCNSS config file, which ends up as a writable
 # version in /data/misc/wifi
 $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-    ln -sf wifi/WCNSS_qcom_cfg.ini \
+    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
         $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 endif
