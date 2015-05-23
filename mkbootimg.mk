8a8b372c6fdad77c18fcb65f6d5e9abe354d9084
scoty755 <scoty755@gmail.com>
2015-05-24 07:19:20 +0900

    mkbootimg: Revamped b2wlj dts of include method

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -8,7 +8,7 @@ LOCAL_PATH := $(call my-dir)
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 b2_DTS_NAMES := msm8974
 
-b2_DTS_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/msm8974-b2wlj.dts)
+b2_DTS_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm/boot/dts/msm8974pro-ab-pm8941-b2wlj*.dts)
 b2_DTS_FILE = $(lastword $(subst /, ,$(1)))
 DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%.dtb,$(call b2_DTS_FILE,$(1))))
 ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/arm/boot/,$(patsubst %.dts,%-zImage,$(call b2_DTS_FILE,$(1))))
